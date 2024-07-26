browser-actions/release-firefox-addon

commit 8c2232194688b81a0f775e015109ff02f75812f2

feat: bump node version (#18)
Bump runtime version to node 20 and use naive fetch() in Node.js.

src/amo.ts
@@ -1,7 +1,6 @@
 import type { ReadStream } from "node:fs";
 import FormData from "form-data";
 import jwt from "jsonwebtoken";
-import fetch from "node-fetch";
 import { v4 as uuidv4 } from "uuid";
 
 type VersionRange = { min?: string; max?: string };

action.yml
@@ -40,5 +40,5 @@ outputs:
     description: 'URL of the edit page of the uploaded version'
 
 runs:
-  using: 'node16'
+  using: 'node20'
   main: 'index.js'

commit 9eb188a8ce93a82ba38960e99c508026422121ad

fix: fix request error (#21)
Fix an issue caused by
https://github.com/browser-actions/release-firefox-addon/pull/18. The
body in the native `fetch()` should be Blob instead of Node API in
Node.js. This PR fix the request body issue.

src/amo.ts
@@ -1,5 +1,3 @@
-import type { ReadStream } from "node:fs";
-import FormData from "form-data";
 import jwt from "jsonwebtoken";
 import { v4 as uuidv4 } from "uuid";
 
@@ -119,12 +117,12 @@ export class AMOClient {
   }
 
   async uploadAddon(
-    xpi: ReadStream,
+    xpi: Blob,
     channel: Channel,
   ): Promise<AMOApiUploadDetailResponse> {
     const path = "/api/v5/addons/upload/";
     const form = new FormData();
-    form.append("upload", xpi);
+    form.append("upload", xpi, "addon.zip");
     form.append("channel", channel);
 
     return this.proceed<AMOApiUploadDetailResponse>(path, "POST", form);
@@ -139,12 +137,12 @@ export class AMOClient {
   async uploadSource(
     addon: number | string,
     version: string,
-    source: ReadStream,
+    source: Blob,
     license?: License,
   ): Promise<VersionDetailResponse> {
     const path = `/api/v5/addons/addon/${addon}/versions/${version}/`;
     const form = new FormData();
-    form.append("source", source);
+    form.append("source", source, "source.zip");
     form.append("license", license);
 
     return this.proceed<VersionDetailResponse>(path, "PATCH", form);

src/index.ts
@@ -41,7 +41,7 @@ async function run(): Promise<void> {
     },
   });
 
-  const addonZip = fs.createReadStream(addonPath);
+  const addonZip = await fs.openAsBlob(addonPath);
   const upload = await client.uploadAddon(addonZip, channel);
 
   core.info(
@@ -86,7 +86,7 @@ async function run(): Promise<void> {
   }
 
   if (sourcePath) {
-    const sourceZip = fs.createReadStream(sourcePath);
+    const sourceZip = await fs.openAsBlob(sourcePath);
     const src = await client.uploadSource(
       addonId,
       version.version,